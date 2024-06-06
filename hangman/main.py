68e041084b2a22b5e1c6390033b5f61f27afd03f
Dana Sharon <whosedreamisthis@gmail.com>
2024-06-06 13:06:19 -0700

    Hangman game

---
+++ hangman/main.py
@@ -0,0 +1,43 @@
+import random
+import hangman_art
+import hangman_words
+
+
+chosen_word = random.choice(hangman_words.word_list)
+print (hangman_art.logo)
+hidden_word = []
+for l in chosen_word:
+    hidden_word.append("_")
+num_lives = 6
+print(" ".join(hidden_word))
+guesses = []
+while("_" in hidden_word and num_lives > 0):
+   guess = input("guess a letter: ").lower()
+   
+   if guess in guesses:
+      print(f"You've already guessed {guess}")
+      print(" ".join(hidden_word))
+   else:
+      i = 0
+      found_letter = False
+      for l in chosen_word:
+         if guess == l:
+            hidden_word[i] = l
+            found_letter = True 
+         i += 1
+      if guess not in chosen_word:
+         num_lives -= 1
+         print("num_lives", num_lives)
+
+      print(" ".join(hidden_word))
+      print(hangman_art.stages[num_lives])
+      guesses.append(guess)
+
+   
+if num_lives > 0:
+   print("You win!")
+else: 
+   print("You lose!")   
+
+
+
