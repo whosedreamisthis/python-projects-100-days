0829c7919c9fa05f4340990bde58d02fa9bfceb4
Dana Sharon <whosedreamisthis@gmail.com>
2024-06-08 13:24:16 -0700

    second paddle

--- pong/main.py
+++ pong/main.py
@@ -1,33 +1,20 @@
-from turtle import Turtle, Screen
-
-
-
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
-paddle = Turtle()
-paddle.shape('square')
-paddle.color('white')
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
-
-def go_up ():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down ():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(go_down, 'Down')
-screen.onkey(go_up, 'Up')
+screen.onkeypress(r_paddle.go_down, 'Down')
+screen.onkeypress(r_paddle.go_up, 'Up')
+
+screen.onkeypress(l_paddle.go_down, 's')
+screen.onkeypress(l_paddle.go_up, 'w')
 
 game_is_on = True
 while game_is_on:
